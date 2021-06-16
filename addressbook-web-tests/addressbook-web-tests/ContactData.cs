4e34de057cd311376ba62d11c265999cefe9dc1d
kdmitrieva16 <dksu82@mail.ru>
2021-06-16 16:01:25 +0300

    Создан тест для дбавления контактов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname="";
+
+        public ContactData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        
+    }
+}
+
